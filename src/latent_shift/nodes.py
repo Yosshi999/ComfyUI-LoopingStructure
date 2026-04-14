f62fee8ed8d6f9063aab894b17e445c6af59816f
Yosshi999 <Yosshi999@users.noreply.github.com>
2026-04-14 23:23:40 +0900

    glued attention

--- src/latent_shift/nodes.py
+++ src/latent_shift/nodes.py
@@ -1,6 +1,24 @@
 from inspect import cleandoc
 from comfy_api.latest import io
 import torch
+from torch import Tensor
+from typing import Tuple, List, Literal
+
+def retrieve_linspace_parameters(samples: Tensor) -> Tuple[Tensor, Tensor, int]:
+    """Helper function to retrieve linspace parameters from img_ids.
+    Args:
+        samples (torch.Tensor): The token indices of shape (batch_size, num_tokens).
+    Returns:
+        start (torch.Tensor): linspace start for each batch (batch_size,).
+        end (torch.Tensor): linspace end for each batch (batch_size,).
+        steps (int): linspace steps for each batch.
+    """
+    
+    distr = torch.unique(samples, dim=1)  # (batch_size, num_unique == steps)
+    start = samples.min(1).values  # (batch_size,)
+    end = samples.max(1).values  # (batch_size,)
+    steps = distr.shape[1]
+    return start, end, steps
 
 class LatentShiftImpl:
     def __init__(self, shift_step, shift_axis):
@@ -14,10 +32,7 @@ class LatentShiftImpl:
         # TODO: distribution of specified direction
         target_direction = 2
         samples = data["img_ids"][:, :, target_direction]
-        distr = torch.unique(samples, dim=1)  # (batch_size, num_unique == steps_w)
-        start = samples.min(1).values  # (batch_size,)
-        end = samples.max(1).values  # (batch_size,)
-        steps = distr.shape[1]
+        start, end, steps = retrieve_linspace_parameters(samples)
         stepsize = (end - start) / (steps - 1)
         # calculate shift amount
         shift_amount = self.shift_step * stepsize * self.call_count  # (batch_size,)
@@ -131,15 +146,126 @@ class VAEDecodeCircular:
             images = images.narrow(1 + i, pad, dim_size - 2 * pad)
         return (images, )
 
+class GluedAttentionImpl:
+    def __init__(self, loop_x: bool, loop_y: bool):
+        self.loop_x = loop_x
+        self.loop_y = loop_y
+
+    def replicate_rope(self, data: dict):
+        img_ids: Tensor = data["img_ids"]  # (batch_size, num_tokens, axes_dim)
+        img_len = img_ids.shape[1]
+        txt_len: int = data["txt_ids"].shape[1]
+        valid_segment: List[Tuple[int, int]] = []  # valid distance of ids
+        for loop, target_direction in [(self.loop_x, 2), (self.loop_y, 1)]:
+            start, end, steps = retrieve_linspace_parameters(img_ids[:, :, target_direction])
+            width = end - start
+            if not loop:
+                valid_segment.append((-width, width))  # full attention
+                continue
+            stepsize = (end - start) / (steps - 1)
+            left_max = start + stepsize * (steps // 2)
+            clone_img_ids = img_ids.clone()
+            clone_img_ids[:, :, target_direction] = torch.where(
+                img_ids[:, :, target_direction] <= left_max[:, None],
+                img_ids[:, :, target_direction] + (stepsize * steps)[:, None],
+                img_ids[:, :, target_direction] - (stepsize * steps)[:, None]
+            )
+            valid_segment.append((-width//2, width//2))  # only allow attention within half loop TODO: boundary case
+            # valid_segment.append((-width//4, width//4))
+            # update img_ids
+            img_ids = torch.cat([img_ids, clone_img_ids], dim=1)
+        out = data.copy()
+        out["img_ids"] = img_ids
+        mask_modifier = torch.ones((1, txt_len + img_len, txt_len + img_ids.shape[1]), device=img_ids.device, dtype=torch.bool)
+        mask_modifier[:, :txt_len, txt_len + img_len:] = False  # prevent text tokens from attending to the glued image tokens
+        # apply distance constraint
+        for loop, target_direction, (left_bound, right_bound) in zip([self.loop_x, self.loop_y], [2, 1], valid_segment):
+            if not loop:
+                continue
+            img_ids_axis = img_ids[:, :, target_direction]
+            diff = img_ids_axis[:, None, :] - img_ids_axis[:, :img_len, None]
+            mask_modifier[:, txt_len:, txt_len:] &= (left_bound <= diff) & (diff <= right_bound)
+        self.mask_modifier = mask_modifier
+        return out
+    
+    def attn_pre(self, q, k, v, pe=None, attn_mask=None, extra_options=None):
+        """
+        q: (batch_size, num_heads, L, dim)
+        k: (batch_size, num_heads, S, dim)
+        v: (batch_size, num_heads, S, dim)
+        """
+        block_type: Literal["single", "double"] = extra_options["block_type"]
+        img_slice: List[int] = extra_options["img_slice"]
+        """
+        Note: https://docs.pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
+        L: query length, S: key/value length, N: batch size
+        attn_mask (optional Tensor): Attention mask; shape must be broadcastable to the shape of attention weights, which is (N,...,L,S).
+        Two types of masks are supported. A boolean mask where a value of True indicates that the element should take part in attention.
+        A float mask of the same type as query, key, value that is added to the attention score.
+        """
+        attn_mask = attn_mask.clone() if attn_mask is not None else torch.ones((1, q.shape[2], k.shape[2]), device=q.device, dtype=torch.bool)
+        assert attn_mask.shape == (1, q.shape[2], k.shape[2]), f"Expected attn_mask shape (1, {q.shape[2]}, {k.shape[2]}), got {attn_mask.shape}"
+        k_glues = []
+        v_glues = []
+        for loop in [self.loop_x, self.loop_y]:
+            if not loop:
+                continue
+            k_img = k[:, :, img_slice[0]:img_slice[1], :]
+            v_img = v[:, :, img_slice[0]:img_slice[1], :]
+            k_glues.append(k_img)
+            v_glues.append(v_img)
+            attn_mask = torch.cat([attn_mask, attn_mask[:, :, img_slice[0]:img_slice[1]]], dim=2)
+
+        out = {
+            "q": q,
+            "k": torch.cat([k, *k_glues], dim=2),
+            "v": torch.cat([v, *v_glues], dim=2),
+            "pe": pe,
+            "attn_mask": attn_mask & self.mask_modifier,
+        }
+        return out
+
+
+class GluedAttention:
+    """Glued Attention for looped structure."""
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": ("MODEL", {"tooltip": "The model to apply the Glued Attention to."}),
+                "loop_x": ("BOOLEAN", {"default": True}),
+                "loop_y": ("BOOLEAN", {"default": True}),
+            },
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "execute"
+    CATEGORY = "model_patches"
+
+    def execute(self, model: io.Model.Type, loop_x: bool, loop_y: bool):
+        model = model.clone()
+        patch_obj = GluedAttentionImpl(loop_x, loop_y)
+        model.set_model_post_input_patch(patch_obj.replicate_rope)
+        model.set_model_attn1_patch(patch_obj.attn_pre)
+        return (model,)
+
+
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "LatentShift": LatentShift,
     "VAEDecodeCircular": VAEDecodeCircular,
+    "GluedAttention": GluedAttention,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LatentShift": "Latent Shift Node",
-    "VAEDecodeCircular": "VAE Decode Circular Node"
+    "VAEDecodeCircular": "VAE Decode Circular Node",
+    "GluedAttention": "Glued Attention Node"
 }
